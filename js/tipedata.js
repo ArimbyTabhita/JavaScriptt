36b1171df28a9875d4e3c8c2949b9b648d7f4f87
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-01-19 19:13:46 +0700

    JavaScript Vid 3

---
+++ js/tipedata.js
@@ -0,0 +1,27 @@
+let angka = 10;
+
+let kata = "Saya belajaar JavaScri8pt";
+
+let pecahan = 2.5;
+
+console.log(angka);
+console.log(typeof pecahan);
+
+console.log(1/0);
+
+console.log("JavaScript" /2+5);
+
+let isi = "Belajar";
+console.log(isi+kata);
+
+console.log(` Smk revit ${angka} ${kata}`);
+console.log("Smk revit"+angka+kata+isi);
+
+let kondisi = true;
+console.log(kondisi);
+
+let age = nul;
+console.log(age);
+
+let kosong;
+console.log(kosong);
