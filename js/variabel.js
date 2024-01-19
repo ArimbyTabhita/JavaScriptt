cfacd73963ef5daf1f1d120888b3d3762d5a7a68
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-01-19 18:59:08 +0700

    JavaScript Vid 2

---
+++ js/variabel.js
@@ -0,0 +1,17 @@
+let angka;
+let kata = "Saya belajar" 
+
+const data = 200;
+
+angka =5;
+
+console.log(angka);
+console.log(kata);
+
+console.log(data);
+
+angka = 5;
+console.log(angka);
+
+let isi = 4.6;
+console.log(typeof isi);
