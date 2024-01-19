7dbb7cf621b6b0b3e12b5320293b0d2063077036
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-01-19 20:24:23 +0700

    JavaScript Vid 7
    Selector Switch Case

---
+++ js/selector.js
@@ -0,0 +1,57 @@
+
+const hari = 4;
+let hasil;
+
+switch (hari) {
+    case 1:
+        hasil = "minggu";
+        break;
+    case 2:
+        hasil = "senin";
+        break;
+    case 3:
+        hasil = "selasa";
+        break;
+    case 4:
+        hasil = "Rabu";
+        break;
+    case 5:
+        hasil = "kamis";
+        break;
+    case 6:
+        hasil = "jumat";
+        break;
+    case 7:
+        hasil = "Sabtu";
+        break;
+
+    default:
+        hasil = "hari tidak ada";
+        break;
+}
+
+console.log(hasil);
+
+const bulan = "Agistus";
+let Month;
+
+switch (Bulan) {
+    case "Januari":
+        bulan = 1
+        break;
+    case "Februari":
+        bulan = 2
+        break;
+    case "Maret":
+        bulan = 3
+        break;
+    case "April":
+        bulan = 4
+        break;
+
+
+    default:
+        bulan = " bulan belum dibuat"
+        break;
+}
+console.log(bulan);
