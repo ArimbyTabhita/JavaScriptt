d1c08b24aadc1e7f12308214d5d121f80ab9ac88
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-01-20 11:33:51 +0700

    JavaScript Vid 13
    DOM

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").innerText = "Saya belajas js";
+
+document.querySelector('p').innerText = "Belajas js itu mudah";
+
+document.querySelector(".isi").innerText = "Saya belajas js di smk revit";
+
+document.querySelector("#judul").innerText = "belajar js itu seru";
